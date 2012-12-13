63aa287dede40ed43dfbe2359b6e5d51d9c70e8e
Sven Kapudija <sven.kapudija@gmail.com>
2012-12-13 22:42:33 +0100

    Added API skeleton

---
+++ src/com/svenkapudija/imagewall/api/ImageWallApi.java
@@ -0,0 +1,124 @@
+package com.svenkapudija.imagewall.api;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+import java.util.Date;
+import java.util.List;
+
+import android.graphics.Bitmap;
+
+import com.google.android.maps.GeoPoint;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.svenkapudija.imagewall.models.Image;
+import com.svenkapudija.imagewall.models.Tag;
+
+public class ImageWallApi {
+
+	private static final String API_BASE_URL = "http://www.domain.com"; 
+	
+	private AsyncHttpClient httpClient;
+	
+	public ImageWallApi() {
+		httpClient = new AsyncHttpClient();
+	}
+	
+	public void getImages(final ImagesListener listener, Date ... lastImageTimestamp) {
+		httpClient.get(API_BASE_URL + "/images", new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(String result) {
+				listener.onSuccess(null);
+			}
+			
+			@Override
+			public void onFailure(Throwable t, String message) {
+				listener.onFailure();
+			}
+		});
+	}
+	
+	public void getImages(Tag tag, final ImagesListener listener) {
+		httpClient.get(API_BASE_URL + "/images?tag=" + encode(tag.getValue()), new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(String result) {
+				listener.onSuccess(null);
+			}
+			
+			@Override
+			public void onFailure(Throwable t, String message) {
+				listener.onFailure();
+			}
+		});
+	}
+	
+	public void getTags(GeoPoint location, final TagsListener listener) {
+		httpClient.get(API_BASE_URL + "/tags", new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(String result) {
+				listener.onSuccess(null);
+			}
+			
+			@Override
+			public void onFailure(Throwable t, String message) {
+				listener.onFailure();
+			}
+		});
+	}
+	
+	public void getImage(int id, final ImageListener listener) {
+		httpClient.get(API_BASE_URL + "/images/" + id, new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(String result) {
+				listener.onSuccess(null);
+			}
+			
+			@Override
+			public void onFailure(Throwable t, String message) {
+				listener.onFailure();
+			}
+		});
+	}
+	
+	public void uploadImage(Bitmap image, final ImageWallListener listener) {
+		httpClient.post(API_BASE_URL + "/images", new AsyncHttpResponseHandler() {
+			@Override
+			public void onSuccess(String result) {
+				listener.onSuccess();
+			}
+			
+			@Override
+			public void onFailure(Throwable t, String message) {
+				listener.onFailure();
+			}
+		});
+	}
+	
+	private String encode(String string) {
+		try {
+			return URLEncoder.encode(string, "UTF-8");
+		} catch (UnsupportedEncodingException ignorable) {}
+		
+		return string;
+	}
+	
+	private interface ImageListener {
+		public void onSuccess(Image image);
+		public void onFailure();
+	}
+	
+	private interface TagsListener {
+		public void onSuccess(List<Tag> tags);
+		public void onFailure();
+	}
+	
+	private interface ImagesListener {
+		public void onSuccess(List<Image> images);
+		public void onFailure();
+	}
+	
+	private interface ImageWallListener {
+		public void onSuccess();
+		public void onFailure();
+	}
+
+}
---
+++ src/com/svenkapudija/imagewall/models/Tag.java
@@ -0,0 +1,15 @@
+package com.svenkapudija.imagewall.models;
+
+public class Tag {
+
+	private String value;
+	
+	public Tag(String value) {
+		this.value = value;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+
+}
